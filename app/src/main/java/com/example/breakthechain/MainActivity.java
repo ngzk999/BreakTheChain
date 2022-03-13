611bd0b279c29929abd365452c8542920ac09c84
ngzk999 <ngzk916@gmail.com>
2022-03-14 05:07:06 +0800

    Malaysia Covid Record able to show now.

--- app/src/main/java/com/example/breakthechain/MainActivity.java
+++ app/src/main/java/com/example/breakthechain/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.breakthechain;
 
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.os.Bundle;
-import android.view.Window;
-
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         chipNavigationBar = findViewById(R.id.nav_menu);
         chipNavigationBar.setItemSelected(R.id.nav_statistics, true);
+        callMyApi();
         getSupportFragmentManager().beginTransaction().replace(R.id.container,new StatisticsFragment()).commit();
         bottomMenu();
     }
@@ -34,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
                 switch (i){
                     case R.id.nav_statistics:
                         fragment = new StatisticsFragment();
+                        callMyApi();
                         break;
                     case R.id.nav_record:
                         fragment = new RecordFragment();
@@ -48,5 +61,41 @@ public class MainActivity extends AppCompatActivity {
                 getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
             }
         });
+    }
+
+    private void callMyApi(){
+        // have to use 10.0.2.2
+        String url = "http://10.0.2.2:5225/api/data/malaysiaLatestData";
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+
+                    TextView malaysiaTitle = findViewById(R.id.idTVMyTitle);
+                    malaysiaTitle.setText("Malaysia Cases - " + response.getString("date"));
+
+                    TextView myNew = findViewById(R.id.idTVMyNew);
+                    myNew.setText(response.getString("newCases"));
+
+                    TextView myRecovered = findViewById(R.id.idTVMyRecovered);
+                    myRecovered.setText(response.getString("recoveredCases"));
+
+                    TextView myDeath = findViewById(R.id.idTVMyDeath);
+                    myDeath.setText(response.getString("deathCases"));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        queue.add(stringRequest);
     }
 }
